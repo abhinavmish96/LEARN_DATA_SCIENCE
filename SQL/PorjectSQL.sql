f97919cf48c6f77791f52c922933a4d941961a37
Abhinav Mishra <abhinavmish96@gmail.com>
2020-05-04 13:16:36 +0530

    learn SQL

--- SQL/PorjectSQL.sql
+++ SQL/PorjectSQL.sql
@@ -8,4 +8,14 @@ CREATE TABLE friends (
 INSERT INTO friends (id, name, birthday)
 VALUES (1, 'Jane Doe', '1990-05-30');
 
+INSERT INTO friends (id, name, birthday)
+VALUES (2, 'BFF One', '1990-01-01');
+
+INSERT INTO friends (id, name, birthday)
+VALUES (3, 'BFF Two', '1990-02-02');
+
+UPDATE friends
+SET name = 'Jane Smith'
+WHERE id = 1;
+
 SELECT * FROM friends;
