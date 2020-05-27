32a6f2c2dc5031106c9a3f71545a4f5ba3e7bfeb
abhinavmish96 <abhinavmish96@gmail.com>
2020-05-27 22:28:51 +0530

    Learn Python

--- PYTHON/Lambda.py
+++ PYTHON/Lambda.py
@@ -3,3 +3,9 @@ contains_a = lambda word: "a" in word
 print contains_a("banana")
 print contains_a("apple")
 print contains_a("cherry")
+
+#Write your lambda function here
+long_string = lambda str: len(str) > 12
+
+print long_string("short")
+print long_string("photosynthesis")
