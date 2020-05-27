9ba3daeabf3203090dd4ec1e2f73dacff2b0448a
abhinavmish96 <abhinavmish96@gmail.com>
2020-05-27 22:39:53 +0530

    Learn Python

--- PYTHON/Lambda.py
+++ PYTHON/Lambda.py
@@ -9,3 +9,9 @@ long_string = lambda str: len(str) > 12
 
 print long_string("short")
 print long_string("photosynthesis")
+
+#Write your lambda function here
+ends_in_a = lambda str: str[len(str) -1 ] == 'a'
+
+print ends_in_a("data")
+print ends_in_a("aardvark")
