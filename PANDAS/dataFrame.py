507788b037a46f9d2ceb9812006658de50c30b31
abhinavmish96 <abhinavmish96@gmail.com>
2020-06-07 14:40:12 +0530

    Learn Pandas

--- PANDAS/dataFrame.py
+++ PANDAS/dataFrame.py
@@ -74,3 +74,17 @@ print(df3)
 df2.reset_index(inplace = True, drop = True)
 
 print(df2)
+
+df = pd.DataFrame([
+  [1, '3 inch screw', 0.5, 0.75],
+  [2, '2 inch nail', 0.10, 0.25],
+  [3, 'hammer', 3.00, 5.50],
+  [4, 'screwdriver', 2.50, 3.00]
+],
+  columns=['Product ID', 'Description', 'Cost to Manufacture', 'Price']
+)
+
+# Add columns here
+df['Sold in Bulk?'] = ['Yes','Yes','No','No']
+
+print(df)
