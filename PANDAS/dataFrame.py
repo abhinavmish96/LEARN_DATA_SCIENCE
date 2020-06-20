e3722c62c6e6381eeabe517b752ffa9db4c30d23
abhinavmish96 <abhinavmish96@gmail.com>
2020-06-20 13:52:07 +0530

    Learn Pandas

--- PANDAS/dataFrame.py
+++ PANDAS/dataFrame.py
@@ -102,3 +102,10 @@ print(df)
 shoe_counts = orders.groupby(['shoe_type','shoe_color'])['id'].count().reset_index()
 
 print(shoe_counts)
+
+shoe_counts_pivot = shoe_counts.pivot(
+    columns='shoe_color',
+    index='shoe_type',
+    values='id').reset_index()
+
+print(shoe_counts_pivot)
