87fe85c3f3b7565c89b5025dbdf71438e5a2870b
abhinavmish96 <abhinavmish96@gmail.com>
2020-06-21 20:57:06 +0530

    Learn Pandas

--- PANDAS/dataFrame.py
+++ PANDAS/dataFrame.py
@@ -109,3 +109,28 @@ shoe_counts_pivot = shoe_counts.pivot(
     values='id').reset_index()
 
 print(shoe_counts_pivot)
+
+user_visits = pd.read_csv('page_visits.csv')
+
+# Part 1.
+print(user_visits.head(10))
+
+# Part 2.
+click_source = user_visits.groupby('utm_source').id.count().reset_index()
+
+#Part 3.
+print(click_source)
+
+#Part 4.
+click_source_by_month = user_visits.groupby(['utm_source', 'month']).id.count().reset_index()
+
+#print(click_source_by_month)
+
+#Part 5.
+click_source_by_month_pivot = click_source_by_month.pivot(
+	columns = 'month',
+	index = 'utm_source',
+	values = 'id').reset_index()
+
+#Part 6.
+print(click_source_by_month_pivot)
