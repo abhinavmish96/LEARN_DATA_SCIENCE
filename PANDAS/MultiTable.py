2b67aeb64ea935c123c13a2aa6f066b76e8645ed
abhinavmish96 <abhinavmish96@gmail.com>
2020-06-27 13:50:42 +0530

    Learn Pandas

--- PANDAS/MultiTable.py
+++ PANDAS/MultiTable.py
@@ -14,3 +14,15 @@ print(customers)
 
 order_3_description = 'thing-a-ma-jig'
 order_5_phone_number = '112-358-1321'
+
+
+sales = pd.read_csv('sales.csv')
+print(sales)
+targets = pd.read_csv('targets.csv')
+print(targets)
+
+sales_vs_targets = pd.merge(sales, targets)
+
+print(sales_vs_targets)
+
+crushing_it = sales_vs_targets[sales_vs_targets.revenue > sales_vs_targets.target]
