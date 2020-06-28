ed1daf8bccc10c5a3e8e7514694a248dff5e86ce
abhinavmish96 <abhinavmish96@gmail.com>
2020-06-28 15:48:14 +0530

    Learn Pandas

--- PANDAS/MultiTable.py
+++ PANDAS/MultiTable.py
@@ -26,3 +26,16 @@ sales_vs_targets = pd.merge(sales, targets)
 print(sales_vs_targets)
 
 crushing_it = sales_vs_targets[sales_vs_targets.revenue > sales_vs_targets.target]
+
+sales = pd.read_csv('sales.csv')
+
+targets = pd.read_csv('targets.csv')
+
+men_women = pd.read_csv('men_women_sales.csv')
+
+all_data = sales.merge(targets)\
+	.merge(men_women)
+
+print(all_data)
+
+results = all_data[(all_data.revenue > all_data.target) & (all_data.women > all_data.men)]
