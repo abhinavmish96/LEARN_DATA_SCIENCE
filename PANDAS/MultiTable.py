a278cb2d906bd6f6598997e17e2ac909639e7006
abhinavmish96 <abhinavmish96@gmail.com>
2020-07-05 23:30:16 +0530

    Learn Pandas

--- PANDAS/MultiTable.py
+++ PANDAS/MultiTable.py
@@ -62,3 +62,12 @@ print(products)
 merged_df = pd.merge(orders, products)
 
 print(merged_df)
+
+store_a = pd.read_csv('store_a.csv')
+print(store_a)
+store_b = pd.read_csv('store_b.csv')
+print(store_b)
+
+store_a_b_outer = pd.merge(store_a, store_b, how='outer')
+
+print(store_a_b_outer)
