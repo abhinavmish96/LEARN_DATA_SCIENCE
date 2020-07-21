b136353aafce2ea8937446db47ebe5971f6590a0
abhinavmish96 <abhinavmish96@gmail.com>
2020-07-21 20:28:16 +0530

    Learn Pandas

--- PANDAS/Matplotlib.py
+++ PANDAS/Matplotlib.py
@@ -7,3 +7,15 @@ money_spent = [10, 12, 12, 10, 14, 22, 24]
 plt.plot(days, money_spent)
 
 plt.show()
+
+import codecademylib
+from matplotlib import pyplot as plt
+
+time = [0, 1, 2, 3, 4]
+revenue = [200, 400, 650, 800, 850]
+costs = [150, 500, 550, 550, 560]
+
+plt.plot(time, revenue)
+plt.plot(time, costs)
+
+plt.show()
