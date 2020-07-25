b9545851cc0be4f0d2e3cceaccfd37961c6bdb54
abhinavmish96 <abhinavmish96@gmail.com>
2020-07-25 23:38:56 +0530

    Learn Pandas

--- PANDAS/Matplotlib.py
+++ PANDAS/Matplotlib.py
@@ -31,3 +31,29 @@ plt.plot(time, revenue, color="purple", linestyle='--')
 plt.plot(time, costs, color="#82edc9", marker='s')
 
 plt.show()
+
+import codecademylib
+from matplotlib import pyplot as plt
+
+x = range(12)
+y = [3000, 3005, 3010, 2900, 2950, 3050, 3000, 3100, 2980, 2980, 2920, 3010]
+plt.plot(x, y)
+
+plt.axis([0, 12, 2900, 3100])
+
+plt.show()
+
+import codecademylib
+from matplotlib import pyplot as plt
+
+months = range(12)
+temperature = [36, 36, 39, 52, 61, 72, 77, 75, 68, 57, 48, 48]
+flights_to_hawaii = [1200, 1300, 1100, 1450, 850, 750, 400, 450, 400, 860, 990, 1000]
+
+plt.subplot(1, 2, 1)
+plt.plot(months, temperature)
+
+plt.subplot(1, 2, 2)
+plt.plot(temperature, flights_to_hawaii, "o")
+
+plt.show()
