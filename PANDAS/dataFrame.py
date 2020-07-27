a5f0b1d0bd5be8d3e2d718e6363c5cfc9e7c054b
abhinavmish96 <abhinavmish96@gmail.com>
2020-07-27 13:50:47 +0530

    Learn Pandas

--- PANDAS/dataFrame.py
+++ PANDAS/dataFrame.py
@@ -134,3 +134,27 @@ click_source_by_month_pivot = click_source_by_month.pivot(
 
 #Part 6.
 print(click_source_by_month_pivot)
+
+import codecademylib
+from matplotlib import pyplot as plt
+
+x = range(7)
+straight_line = [0, 1, 2, 3, 4, 5, 6]
+parabola = [0, 1, 4, 9, 16, 25, 36]
+cubic = [0, 1, 8, 27, 64, 125, 216]
+
+# Subplot 1
+plt.subplot(2, 1, 1)
+plt.plot(x, straight_line)
+
+# Subplot 2
+plt.subplot(2, 2, 3)
+plt.plot(x, parabola)
+
+# Subplot 3
+plt.subplot(2, 2, 4)
+plt.plot(x, cubic)
+
+plt.subplots_adjust(wspace=0.35, bottom=0.2)
+
+plt.show()
