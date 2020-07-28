ec25fa9d77027882eae26143d18de7dc76b09a47
abhinavmish96 <abhinavmish96@gmail.com>
2020-07-28 11:10:25 +0530

    Learn Matplotlib

--- PANDAS/dataFrame.py
+++ PANDAS/dataFrame.py
@@ -158,3 +158,21 @@ plt.plot(x, cubic)
 plt.subplots_adjust(wspace=0.35, bottom=0.2)
 
 plt.show()
+
+import codecademylib
+from matplotlib import pyplot as plt
+
+months = range(12)
+hyrule = [63, 65, 68, 70, 72, 72, 73, 74, 71, 70, 68, 64]
+kakariko = [52, 52, 53, 68, 73, 74, 74, 76, 71, 62, 58, 54]
+gerudo = [98, 99, 99, 100, 99, 100, 98, 101, 101, 97, 98, 99]
+
+plt.plot(months, hyrule)
+plt.plot(months, kakariko)
+plt.plot(months, gerudo)
+
+#create your legend here
+legend_labels = ["Hyrule", "Kakariko", "Gerudo Valley"]
+plt.legend(legend_labels, loc=8)
+
+plt.show()
